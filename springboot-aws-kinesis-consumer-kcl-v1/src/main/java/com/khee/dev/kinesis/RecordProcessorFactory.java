630d11f9c143e888affe16db554b870bda4f2a4a
KheeForDev <52060358+KheeForDev@users.noreply.github.com>
2020-09-21 12:22:19 +0800

    UPD: Auto-format

--- springboot-aws-kinesis-consumer-kcl-v1/src/main/java/com/khee/dev/kinesis/RecordProcessorFactory.java
+++ springboot-aws-kinesis-consumer-kcl-v1/src/main/java/com/khee/dev/kinesis/RecordProcessorFactory.java
@@ -3,7 +3,7 @@ package com.khee.dev.kinesis;
 import com.amazonaws.services.kinesis.clientlibrary.interfaces.v2.IRecordProcessor;
 import com.amazonaws.services.kinesis.clientlibrary.interfaces.v2.IRecordProcessorFactory;
 
-public class RecordProcessorFactory  implements IRecordProcessorFactory {
+public class RecordProcessorFactory implements IRecordProcessorFactory {
 
 	@Override
 	public IRecordProcessor createProcessor() {
