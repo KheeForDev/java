218c19c7faf7ddf05b54944d88f053a49bdaad8d
KheeForDev <52060358+KheeForDev@users.noreply.github.com>
2020-11-12 12:39:32 +0800

    Initial commit for aws firehose application.

---
+++ springboot-aws-firehose-producer-sdk/src/main/java/com/khee/dev/kinesis/FirehoseProducer.java
@@ -0,0 +1,54 @@
+package com.khee.dev.kinesis;
+
+import java.nio.ByteBuffer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehose;
+import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseClientBuilder;
+import com.amazonaws.services.kinesisfirehose.model.PutRecordRequest;
+import com.amazonaws.services.kinesisfirehose.model.PutRecordResult;
+import com.amazonaws.services.kinesisfirehose.model.Record;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.khee.dev.model.Properties;
+
+@Component
+public class FirehoseProducer {
+	private static final Logger log = LogManager.getLogger(FirehoseProducer.class);
+
+	@Autowired
+	private Properties properties;
+
+	public void putIntoFirehose(String payload) {
+		AmazonKinesisFirehoseClientBuilder clientBuilder = AmazonKinesisFirehoseClientBuilder.standard();
+
+		BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(properties.getAwsFirehoseAccessKey(),
+				properties.getAwsFirehoseSecretKey());
+
+		clientBuilder.setCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials));
+		clientBuilder.setRegion(properties.getAwsFirehoseRegion());
+		clientBuilder.setClientConfiguration(new ClientConfiguration());
+
+		AmazonKinesisFirehose firehoseClient = clientBuilder.build();
+
+		PutRecordRequest putRecordRequest = new PutRecordRequest();
+		putRecordRequest.setDeliveryStreamName(properties.getAwsFirehoseStream());
+		putRecordRequest.setRecord(new Record().withData(ByteBuffer.wrap(String.valueOf(payload).getBytes())));
+
+		PutRecordResult putRecordResult = firehoseClient.putRecord(putRecordRequest);
+
+		try {
+			log.info("Put Result {}",
+					new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(putRecordResult));
+		} catch (JsonProcessingException e) {
+			log.error(e.getMessage());
+		}
+	}
+}
---
+++ springboot-aws-firehose-producer-sdk/src/main/java/com/khee/dev/model/Properties.java
@@ -0,0 +1,51 @@
+package com.khee.dev.model;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Properties {
+	@Value("${aws.firehose.region}")
+	private String awsFirehoseRegion;
+
+	@Value("${aws.firehose.stream}")
+	private String awsFirehoseStream;
+
+	@Value("${aws.firehose.access.key}")
+	private String awsFirehoseAccessKey;
+
+	@Value("${aws.firehose.secret.key}")
+	private String awsFirehoseSecretKey;
+
+	public String getAwsFirehoseRegion() {
+		return awsFirehoseRegion;
+	}
+
+	public void setAwsFirehoseRegion(String awsFirehoseRegion) {
+		this.awsFirehoseRegion = awsFirehoseRegion;
+	}
+
+	public String getAwsFirehoseStream() {
+		return awsFirehoseStream;
+	}
+
+	public void setAwsFirehoseStream(String awsFirehoseStream) {
+		this.awsFirehoseStream = awsFirehoseStream;
+	}
+
+	public String getAwsFirehoseAccessKey() {
+		return awsFirehoseAccessKey;
+	}
+
+	public void setAwsFirehoseAccessKey(String awsFirehoseAccessKey) {
+		this.awsFirehoseAccessKey = awsFirehoseAccessKey;
+	}
+
+	public String getAwsFirehoseSecretKey() {
+		return awsFirehoseSecretKey;
+	}
+
+	public void setAwsFirehoseSecretKey(String awsFirehoseSecretKey) {
+		this.awsFirehoseSecretKey = awsFirehoseSecretKey;
+	}
+}
