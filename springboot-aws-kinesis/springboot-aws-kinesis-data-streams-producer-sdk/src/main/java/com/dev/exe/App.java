df26726cf94a6b8b1b2bfa61d3055f8fb2cc1668
KheeForDev <52060358+KheeForDev@users.noreply.github.com>
2020-12-11 15:22:47 +0800

    UPD: Update log4j2.xml and ComponentScan to the updated package structure.

--- springboot-aws-kinesis/springboot-aws-kinesis-data-streams-producer-sdk/src/main/java/com/dev/exe/App.java
+++ springboot-aws-kinesis/springboot-aws-kinesis-data-streams-producer-sdk/src/main/java/com/dev/exe/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.khee.dev")
+@ComponentScan(basePackages = "com.dev")
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
