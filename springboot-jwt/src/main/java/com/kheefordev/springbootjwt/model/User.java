bff68e9781736dcc97899d8fe0dad86994790b8d
Khee Houy, Tan <kheefordev@gmail.com>
2022-05-29 09:34:03 +0800

    UPD: Add new POJO, RoleToUser, and added constructor to existing POJO.

--- springboot-jwt/src/main/java/com/kheefordev/springbootjwt/model/User.java
+++ springboot-jwt/src/main/java/com/kheefordev/springbootjwt/model/User.java
@@ -21,6 +21,18 @@ public class User {
 	@ManyToMany(fetch = FetchType.EAGER)
 	private Collection<Role> roles = new ArrayList<>();
 
+	public User() {
+
+	}
+
+	public User(Long id, String name, String username, String password, ArrayList<Role> roles) {
+		this.id = id;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+
 	public Long getId() {
 		return id;
 	}
