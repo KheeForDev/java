b9fb46048fc5a84c285c5aff2bd5c7461fde2882
Khee Houy, Tan <kheefordev@gmail.com>
2022-05-29 09:35:40 +0800

    UPD: Added annotation to repository class and update request mapping in controller class.

--- springboot-jwt/src/main/java/com/kheefordev/springbootjwt/controller/UserController.java
+++ springboot-jwt/src/main/java/com/kheefordev/springbootjwt/controller/UserController.java
@@ -9,24 +9,45 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kheefordev.springbootjwt.model.Role;
+import com.kheefordev.springbootjwt.model.RoleToUser;
 import com.kheefordev.springbootjwt.model.User;
 import com.kheefordev.springbootjwt.service.UserService;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api")
 public class UserController {
 	private static final Logger log = LogManager.getLogger(UserController.class);
-	
+
 	@Autowired
 	private UserService userService;
-	
-	@GetMapping("/getAll")
+
+	@GetMapping("/users")
 	public ResponseEntity<List<User>> getUsers() {
 		List<User> users = userService.getUsers();
 		return ResponseEntity.status(HttpStatus.OK).body(users);
+	}
+
+	@PostMapping("/user/save")
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(user));
+	}
+
+	@PostMapping("/role/save")
+	public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveRole(role));
+	}
+
+	@PostMapping("/role/addtouser")
+	public ResponseEntity<String> saveRole(@RequestBody RoleToUser roleToUser) {
+		userService.addRoleToUser(roleToUser.getUsername(), roleToUser.getRoleName());
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 }
