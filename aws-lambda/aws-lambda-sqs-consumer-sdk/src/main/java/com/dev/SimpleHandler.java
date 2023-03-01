20bc91b55bda20057122e20da00bf85acd19b148
Khee Houy, Tan <Kheehouy_Tan@singaporeair.com.sg>
2023-03-01 17:23:06 +0800

    UPD: Handle partial successful message and send un-successful message to SQS DLQ.

--- aws-lambda/aws-lambda-sqs-consumer-sdk/src/main/java/com/dev/SimpleHandler.java
+++ aws-lambda/aws-lambda-sqs-consumer-sdk/src/main/java/com/dev/SimpleHandler.java
@@ -11,28 +11,44 @@ import com.dev.model.MessageDto;
 import com.google.gson.Gson;
 
 public class SimpleHandler {
-	public SQSBatchResponse handleRequest(SQSEvent input, Context context) {
+	public void handleRequest(SQSEvent input, Context context) {
+		LambdaLogger logger = context.getLogger();
+
+		logger.log("Number of record(s) to process: " + String.valueOf(input.getRecords().size()));
+
+		input.getRecords().forEach(record -> {
+			String msg = record.getBody();
+			logger.log(msg);
+		});
+	}
+
+	/**
+	 * <p>
+	 * This method is used to handle batch record(s) with partial successful
+	 * message. Using SQSBatchResponse to return messageId that failed to process
+	 * and put into a Dead-Letter Queue (DQL) for further processing or
+	 * investigation.
+	 * </p>
+	 */
+	public SQSBatchResponse handleRequestWithFailure(SQSEvent input, Context context) {
 		LambdaLogger logger = context.getLogger();
 		List<SQSBatchResponse.BatchItemFailure> batchItemFailureList = new ArrayList<>();
 
 		logger.log("Number of record(s) to process: " + String.valueOf(input.getRecords().size()));
 
 		input.getRecords().forEach(record -> {
-			try {
+			String msg = record.getBody();
+			logger.log(msg);
 
-				String msg = record.getBody();
-				logger.log(msg);
+			MessageDto messageDto = new Gson().fromJson(msg, MessageDto.class);
 
-				MessageDto messageDto = new Gson().fromJson(msg, MessageDto.class);
-
-				if (messageDto.getName().toLowerCase().contains("fail")) {
-					throw new RuntimeException("Unable to process message");
-				}
-			} catch (RuntimeException rte) {
-				logger.log("[ERROR]: " + rte.getMessage());
+			if (messageDto.getName().toLowerCase().contains("fail")) {
 				batchItemFailureList.add(new SQSBatchResponse.BatchItemFailure(record.getMessageId()));
+				throw new RuntimeException("Unable to process message");
 			}
 		});
+
+		logger.log("Number of failed record(s): " + String.valueOf(batchItemFailureList.size()));
 
 		return new SQSBatchResponse(batchItemFailureList);
 	}
