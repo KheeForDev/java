934c52ceeb8612652e6a2f0b9a0cfc5c12f09aa4
Khee Houy, Tan <Kheehouy_Tan@singaporeair.com.sg>
2023-03-23 14:56:03 +0800

    UPD: Rename app folder, remove aws credential from properties file.

--- springboot-aws-sqs/springboot-aws-sqs-consumer/src/main/java/com/dev/App.java
+++ springboot-aws-sqs/springboot-aws-sqs-consumer/src/main/java/com/dev/App.java
@@ -2,8 +2,10 @@ package com.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableSqs
 @SpringBootApplication
 @ComponentScan(basePackages = "com.dev")
 public class App {
